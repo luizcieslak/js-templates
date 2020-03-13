bc10190c1a0024a60f6956227ca78c0d8062f6c5
Luiz <cieslakluiz@gmail.com>
2020-03-13 17:56:42 -0300

    chore(gatsby-tailwind): change class to className

--- packages/gatsby-tailwind/src/pages/index.tsx
+++ packages/gatsby-tailwind/src/pages/index.tsx
@@ -15,12 +15,12 @@ const IndexPage: React.FC<IProps> = ({ data }) => {
 	return (
 		<Layout>
 			<SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
-			<div class='w-1/3'>
+			<div className='w-1/3'>
 				{/* TODO: add optional chaining in file */}
 				<GatsbyImage fluid={data.file.childImageSharp.fluid} />
 			</div>
 			<Link to='/page-2'>
-				<button class='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-5'>
+				<button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-5'>
 					go to page 2
 				</button>
 			</Link>
--- packages/gatsby-tailwind/src/pages/page-2.tsx
+++ packages/gatsby-tailwind/src/pages/page-2.tsx
@@ -7,10 +7,10 @@ import SEO from '../components/seo'
 const SecondPage = () => (
 	<Layout>
 		<SEO title='Page two' />
-		<h1 class='tracking-widest'>Hi from the second page</h1>
-		<p class='italic'>Welcome to page 2</p>
+		<h1 className='tracking-widest'>Hi from the second page</h1>
+		<p className='italic'>Welcome to page 2</p>
 		<Link to='/'>
-			<button class='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+			<button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
 				Go back to the homepage
 			</button>
 		</Link>
