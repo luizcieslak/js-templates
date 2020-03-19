5e2b080c2d9778826d2517f1416f4d7d53e65c44
luizcieslak <cieslakluiz@gmail.com>
2020-03-19 20:03:59 -0300

    fix(cra): remove breaking imports

--- packages/cra/src/pages/NotFound.tsx
+++ packages/cra/src/pages/NotFound.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
-import Layout from '../commons/Layout'
 
 export default function NotFound(props: RouteComponentProps) {
-	return (
-		<Layout>
-			<h1>404 :(</h1>
-		</Layout>
-	)
+	return <h1>404 :(</h1>
 }
